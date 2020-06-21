57009a83395635ffdccec43a098564708274801f
sojoud <sahmad@madfooat.com>
2020-06-21 12:12:02 +0300

    starting commits

---
+++ Example/Pods/Target Support Files/PodTesting/PodTesting-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PodTesting : NSObject
+@end
+@implementation PodsDummy_PodTesting
+@end
---
+++ Example/Pods/Target Support Files/Pods-PodTesting_Tests/Pods-PodTesting_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PodTesting_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_PodTesting_Tests
+@end
